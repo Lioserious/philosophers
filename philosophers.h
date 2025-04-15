66b8c19119bd2e2f4ab99f76f03eeb2e30cceb71
Lioserious <lihrig@student.42heilbronn.de>
2025-04-15 17:10:37 +0200

    struct for philosophers

--- philosophers.h
+++ philosophers.h
@@ -6,8 +6,20 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 16:03:25 by lihrig            #+#    #+#             */
-/*   Updated: 2025/04/14 16:37:23 by lihrig           ###   ########.fr       */
+/*   Updated: 2025/04/15 17:10:09 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <pthread.h>
+
+typedef struct s_philosophers
+{
+	int				id;
+	pthread_t		thread;
+	int				le_fork_index;
+	int				re_fork_index;
+	long			last_meal;
+	struct s_data	*data;
+
+}				t_philosophers;
