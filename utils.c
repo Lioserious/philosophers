79a84c79dc2a5f867ba9853d3204fe4e0f414172
Lioserious <lihrig@student.42heilbronn.de>
2025-04-16 18:18:10 +0200

    Initilising steht

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 16:18:06 by lihrig            #+#    #+#             */
-/*   Updated: 2025/04/16 16:55:35 by lihrig           ###   ########.fr       */
+/*   Updated: 2025/04/16 18:17:54 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,46 @@
 t_data *init_data(int argc, char **argv)
 {
 	t_data *data;
+	int i;
 	
+	i = 0;
 	data = malloc(sizeof(t_data));
 	if(!data)
 		NULL;
+	data->nbr_philosophers = atoi(argv[1]);
+	data->time_to_die = atoi(argv[2]);
+	data->time_to_eat = atoi(argv[3]);
+	data->time_to_sleep = atoi(argv[4]);
+	// data->forced_to_eat = atoi(argv[5]); hier muss man nochmal ran vlt mit if statement
+	data->is_dead = 0;
+	data->start_time = get_current_time();
+	data->forks = malloc(sizeof(pthread_mutex_t) * data->nbr_philosophers);
+	if(!data->forks)
+		return(NULL);
+	while(i < data->nbr_philosophers)
+		pthread_mutex_init(&data->forks[i++],NULL);
+	pthread_mutex_init(&data->write_mutex,NULL);
+	pthread_mutex_init(&data->dead_mutex, NULL);
+	return(data);
+}
+t_philosophers *init_philosophers(t_data *data)
+{
+	t_philosophers *philosophers;
+	int i;
+	
+	i = 0;
+	philosophers =malloc(sizeof(t_philosophers) * data->nbr_philosophers);
+	if(!philosophers)
+		return (NULL);
+	while(i < data->nbr_philosophers)
+	{
+		philosophers[i].id = i;
+		philosophers[i].le_fork_index = i;
+		philosophers[i].re_fork_index = (i + 1) % data->nbr_philosophers;
+		philosophers[i].last_meal = data->start_time;
+		philosophers[i].meals_eaten = 0;
+		philosophers[i].data = data;
+		i++;
+	}
+	return(philosophers);
 }
