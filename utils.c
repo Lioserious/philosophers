d334c275d74b6cc4db0d15e84c895ee5905a1f43
Lioserious <lihrig@student.42heilbronn.de>
2025-04-16 17:05:33 +0200

    start to initialize

--- utils.c
+++ utils.c
@@ -6,9 +6,17 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 16:18:06 by lihrig            #+#    #+#             */
-/*   Updated: 2025/04/15 13:55:01 by lihrig           ###   ########.fr       */
+/*   Updated: 2025/04/16 16:55:35 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
+t_data *init_data(int argc, char **argv)
+{
+	t_data *data;
+	
+	data = malloc(sizeof(t_data));
+	if(!data)
+		NULL;
+}
