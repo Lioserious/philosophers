1bb586a13fa4ef349a3f01fa43553ee7aac7526b
Lioserious <lihrig@student.42heilbronn.de>
2025-04-18 11:15:49 +0200

    philosophers added eaten

--- philosophers.h
+++ philosophers.h
@@ -6,7 +6,7 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 16:03:25 by lihrig            #+#    #+#             */
-/*   Updated: 2025/04/15 17:26:06 by lihrig           ###   ########.fr       */
+/*   Updated: 2025/04/16 17:29:41 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ typedef struct s_philosophers
 	int				le_fork_index;
 	int				re_fork_index;
 	long			last_meal;
+	int				meals_eaten;
 	struct s_data	*data;
 
 }				t_philosophers;
