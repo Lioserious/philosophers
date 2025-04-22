aaebb526e825594388c60f9593c2c68fad5284e5
Lioserious <lihrig@student.42heilbronn.de>
2025-04-22 14:37:31 +0200

    Pruefung ob Struct init ist before free, to avoid freeing memory that has been freed

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 16:03:06 by lihrig            #+#    #+#             */
-/*   Updated: 2025/04/22 13:46:41 by lihrig           ###   ########.fr       */
+/*   Updated: 2025/04/22 14:36:30 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,32 @@
 void print_instructions()
 {
 	printf("The Programm takes the arguments in the following Order\n");
-	printf("programm [Number of Philosophers] [time to die] [time to eat] [time to sleep] Optional:[meals he has to eat]\n");
+	printf("programm [Number of Philosophers] [time to die] [time to eat]");
+	printf(" [time to sleep] Optional:[meals he has to eat]\n");
 }
 void free_data(t_data *data)
 {
 	int i;
+	if(!data)
+		return;
 	if(data->forks)
 	{
 		i = 0;
 		while(i < data->nbr_philosophers)
 		{
 			pthread_mutex_destroy(&data->forks[i]);
-			free(data->forks);
 			i++;
 		}
+		free(data->forks);
 	}
-	pthread_attr_destroy(&data->write_mutex);
+	pthread_mutex_destroy(&data->write_mutex);
 	pthread_mutex_destroy(&data->dead_mutex);
 	free(data);
 }
 void free_philisophers(t_philosophers *philosophers)
 {
+	if(!philosophers)
+		return;
 	free(philosophers);
 }
 
@@ -50,7 +55,7 @@ int main (int argc, char* argv[])
 			return(free_data(data), 1);
 		philosophers = init_philosophers(data);
 		if(!philosophers)
-			return(free_philisophers(philosophers), 1);
+			return(free_data(data),free_philisophers(philosophers), 1);
 	}
 	free_philisophers(philosophers);
 	free_data(data);
