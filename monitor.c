c868477ecc01cfe9b42a5a29188316b0571cce33
Lionserious_ <ihrig.leon@gmail.com>
2025-04-27 15:59:26 +0200

    monitor_routine

--- monitor.c
+++ monitor.c
@@ -3,40 +3,80 @@
 /*                                                        :::      ::::::::   */
 /*   monitor.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 17:38:41 by lihrig            #+#    #+#             */
-/*   Updated: 2025/04/23 17:51:27 by lihrig           ###   ########.fr       */
+/*   Updated: 2025/04/27 15:58:55 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-void *monitor_routine(void *arg)
+int check_if_philosopher_died(t_philosophers *philosophers, int index)
 {
-    t_philosophers *philosophers;
+    t_data *data;
+    
+    data = philosophers[0].data;
+    pthread_mutex_lock(&data->dead_mutex);
+    if ((get_current_time() - philosophers[index].last_meal) > data->time_to_die)
+    {
+        data->is_dead = 1;
+        print_status(&philosophers[index], "died");
+        pthread_mutex_unlock(&data->dead_mutex);
+        return (1);
+    }
+    pthread_mutex_unlock(&data->dead_mutex);
+    return (0);
+}
+
+int check_if_all_ate_enough(t_philosophers *philosophers)
+{
     t_data *data;
     int i;
     int all_ate_enough;
     
-    philosophers = (t_philosophers *)arg;
     data = philosophers[0].data;
+    i = 0;
+    all_ate_enough = 1;
+    
+    if (data->forced_to_eat <= 0)
+        return (0);
+    while (i < data->nbr_philosophers)
+    {
+        pthread_mutex_lock(&data->dead_mutex);
+        if (philosophers[i].meals_eaten < data->forced_to_eat)
+            all_ate_enough = 0;
+        pthread_mutex_unlock(&data->dead_mutex);
+        i++;
+    }
+    pthread_mutex_lock(&data->dead_mutex);
+    if (all_ate_enough)
+    {
+        data->is_dead = 1;
+        return (pthread_mutex_unlock(&data->dead_mutex),1);
+    }
+    return (pthread_mutex_unlock(&data->dead_mutex), 0);
+}
+
+void *monitor_routine(void *arg)
+{
+    t_philosophers *philosophers;
+    int i;
+    
+    philosophers = (t_philosophers *)arg;
     
     while (1)
-	{
-		i = 0;
-		all_ate_enough = 1;
-		while(i < data->nbr_philosophers)
-		{
-			pthread_mutex_lock(data->dead_mutex);
-			if ((get_current_time() - philosophers[i].last_meal) > data->time_to_die)
-			{
-				data->is_dead = 1;
-				print_status(&philosophers[i])
-			}
-		}
-		
-	}
-
-	
+    {
+        i = 0;
+        while (i < philosophers[0].data->nbr_philosophers)
+        {
+            if (check_if_philosopher_died(philosophers, i))
+                return (NULL);
+            i++;
+        }
+        if (check_if_all_ate_enough(philosophers))
+            return (NULL);
+        usleep(1000);
+    }
+    return (NULL);
 }
