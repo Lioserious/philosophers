d7579aef190c2da6418c2276683c62e66d9b2bb0
Lioserious <lihrig@student.42heilbronn.de>
2025-05-24 13:40:22 +0200

    monitor.c had to split function

--- monitor.c
+++ monitor.c
@@ -6,7 +6,7 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 17:38:41 by lihrig            #+#    #+#             */
-/*   Updated: 2025/05/24 13:14:27 by lihrig           ###   ########.fr       */
+/*   Updated: 2025/05/24 13:38:39 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,41 +36,55 @@ int check_if_philosopher_died(t_philosophers *philosophers, int index)
     return (0);
 }
 
-int check_if_all_ate_enough(t_philosophers *philosophers)
+static void print_completion_message(t_data *data)
 {
-    t_data *data;
-    int i;
-    int all_ate_enough;
     long current_time;
     long elapsed_time;
     
+    pthread_mutex_lock(&data->write_mutex);
+    current_time = get_current_time();
+    elapsed_time = current_time - data->start_time;
+    printf("%ld All philosophers have eaten %d times each. Simulation complete!\n",
+           elapsed_time, data->forced_to_eat);
+    pthread_mutex_unlock(&data->write_mutex);
+}
+
+/**
+ * Marks the simulation as complete and triggers termination.
+ * Sets the global death flag and prints completion message.
+ */
+static void end_simulation_successfully(t_data *data)
+{
+    data->is_dead = 1;
+    print_completion_message(data);
+}
+
+/**
+ * Checks if all philosophers have eaten the required number of meals.
+ * Returns 1 if simulation should end due to completion, 0 if it should continue.
+ * Handles the case where no meal limit is set (forced_to_eat <= 0).
+ */
+int check_if_all_ate_enough(t_philosophers *philosophers)
+{
+    t_data *data;
+    int satisfied_count;
+    
     data = philosophers[0].data;
-    i = 0;
-    all_ate_enough = 1;
+    
     if (data->forced_to_eat <= 0)
         return (0);
-    while (i < data->nbr_philosophers)
-    {
-        pthread_mutex_lock(&data->dead_mutex);
-        if (philosophers[i].meals_eaten < data->forced_to_eat)
-            all_ate_enough = 0;
-        pthread_mutex_unlock(&data->dead_mutex);
-        i++;
-    }
+    
+    satisfied_count = count_satisfied_philosophers(philosophers, data);
+    
     pthread_mutex_lock(&data->dead_mutex);
-    if (all_ate_enough)
+    if (satisfied_count == data->nbr_philosophers)
     {
-        data->is_dead = 1;
-        pthread_mutex_lock(&data->write_mutex);
-        current_time = get_current_time();
-        elapsed_time = current_time - data->start_time;
-        printf("%ld ALLE PHILOSOPHERS SIND SATT %d times each. Simulation complete!\n", 
-               elapsed_time, data->forced_to_eat);
-        pthread_mutex_unlock(&data->write_mutex);
-        
-        return (pthread_mutex_unlock(&data->dead_mutex),1);
+        end_simulation_successfully(data);
+        pthread_mutex_unlock(&data->dead_mutex);
+        return (1);
     }
-    return (pthread_mutex_unlock(&data->dead_mutex), 0);
+    pthread_mutex_unlock(&data->dead_mutex);
+    return (0);
 }
 
 void *monitor_routine(void *arg)
